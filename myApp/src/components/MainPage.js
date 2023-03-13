2b7b911e63e5a9691de2b729e30e73ee8886f8c2
Good Do <56465252+genegods@users.noreply.github.com>
2023-03-13 15:31:28 +0800

    Add files via upload

---
+++ myApp/src/components/MainPage.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+
+
+const MainPage = () => {
+
+   
+  return (
+    <React.Fragment>
+    <main>
+        <div className="centered h-screen">
+            <div className="shape h-80 w-52 bg-red-500 relative "></div>
+            <div className=" h-80 w-52 bg-white"></div>
+        </div>
+    </main>
+  </React.Fragment>
+  )
+}
+
+export default MainPage
